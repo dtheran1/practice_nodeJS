4d0acb6f4fc66777b2478a2984c846cc8257fdaa
Dtheran <dtheran1@gmail.com>
2023-02-06 20:42:24 -0500

    conectando con mongo db atlas

--- proyect/projectApi/src/database/index.js
+++ proyect/projectApi/src/database/index.js
@@ -1,18 +1,20 @@
 const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:module-database');
 
+const { Config: { mongoDbname, mongoUri } } = require('../config/index');
 
 var connection = null;
 module.exports.Database = (collection) => new Promise(async (resolve, reject) => {
   try {
     if (!connection) {
-      const client = new MongoClient();
+      const client = new MongoClient(mongoUri);
       connection = await client.connect()
       debug('Nueva Conexion realizada con mongoDB Atlas')
     }
-    const db = connection.db()
-    
+    debug('Reutilizando connection')
+    const db = connection.db(mongoDbname)
+    resolve(db.collection(collection))
   } catch (error) {
-    
+    reject(error)
   }
 })
