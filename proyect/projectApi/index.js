6ec9926905d21204afe284aa9e83d280e711b657
Dtheran <dtheran1@gmail.com>
2023-02-08 13:03:45 -0500

    wip

--- proyect/projectApi/index.js
+++ proyect/projectApi/index.js
@@ -5,13 +5,15 @@ const app = express()
 const { Config: { port } } = require('./src/config/index');
 const { ProductsAPI } = require('./src/products/index');
 const { UsersAPI } = require('./src/users/index');
+const { IndexAPI, NotFoundAPI } = require('./src/index/index');
 
 app.use(express.json()) // con esto le damos la capacidad de recibir datos en el req
 
 // Modulos
+IndexAPI(app);
 ProductsAPI(app);
 UsersAPI(app);
-
+NotFoundAPI(app)
 
 app.listen(port, () => {
   debug(`Servidor escuchando en el puerto ${port}`)
